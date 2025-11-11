3305f4382e7838ad13c592823088ae009dc480a9
Pofkinas <povilaszvi358@gmail.com>
2025-11-11 19:29:01 +0200

    feat(base/timer): export timer configuration

--- Source/Driver/timer_driver.c
+++ Source/Driver/timer_driver.c
@@ -4,8 +4,7 @@
 
 #include "timer_driver.h"
 
-#include "stm32f4xx_ll_bus.h"
-#include "stm32f4xx_ll_tim.h"
+#ifdef ENABLE_TIMER
 
 /**********************************************************************************************************************
  * Private definitions and macros
@@ -15,106 +14,17 @@
  * Private typedef
  *********************************************************************************************************************/
 
-typedef struct sTimerDesc {
-    TIM_TypeDef *periph;
-    uint16_t prescaler;
-    uint32_t counter_mode;
-    uint32_t auto_reload;
-    uint32_t clock_division;
-    void (*enable_clock_fp) (uint32_t);
-    uint32_t clock;
-    void (*clock_source_fp) (TIM_TypeDef *, uint32_t);
-    uint32_t clock_source;
-    bool enable_interupt;
-    IRQn_Type nvic;
-    void (*auto_relead_preload_fp) (TIM_TypeDef *);
-    void (*master_slave_mode_fp) (TIM_TypeDef *);
-    void (*set_slave_mode_fp) (TIM_TypeDef *, uint32_t);
-    uint32_t slave_mode;
-    void (*set_trigger) (TIM_TypeDef *, uint32_t);
-    uint32_t triger_sync;
-} sTimerDesc_t;
-
 /**********************************************************************************************************************
  * Private constants
  *********************************************************************************************************************/
-
-/* clang-format off */
-const static sTimerDesc_t g_static_timer_lut[eTimerDriver_Last] = {
-    [eTimerDriver_TIM10] = {
-        .periph = TIM10,
-        .prescaler = 999,
-        .counter_mode = LL_TIM_COUNTERMODE_UP,
-        .auto_reload = 65535,
-        .clock_division = LL_TIM_CLOCKDIVISION_DIV1,
-        .clock = LL_APB2_GRP1_PERIPH_TIM10,
-        .clock_source_fp = NULL,
-        .enable_clock_fp = LL_APB2_GRP1_EnableClock,
-        .nvic = TIM1_UP_TIM10_IRQn,
-        .enable_interupt = true,
-        .auto_relead_preload_fp = LL_TIM_DisableARRPreload,
-        .master_slave_mode_fp = NULL
-    },
-    
-    #if defined(USE_MOTOR_A) || defined(USE_MOTOR_B) || defined(USE_PULSE_LED)
-    [eTimerDriver_TIM3] = {
-        .periph = TIM3,
-        .prescaler = 0,
-        .counter_mode = LL_TIM_COUNTERMODE_UP,
-        .auto_reload = 256,
-        .clock_division = LL_TIM_CLOCKDIVISION_DIV1,
-        .enable_clock_fp = LL_APB1_GRP1_EnableClock,
-        .clock = LL_APB1_GRP1_PERIPH_TIM3,
-        .clock_source_fp = LL_TIM_SetClockSource,
-        .clock_source = LL_TIM_CLOCKSOURCE_INTERNAL,
-        .enable_interupt = false,
-        .auto_relead_preload_fp = LL_TIM_DisableARRPreload,
-        .master_slave_mode_fp = LL_TIM_DisableMasterSlaveMode,
-        .set_trigger = LL_TIM_SetTriggerOutput,
-        .triger_sync = LL_TIM_TRGO_RESET
-    },
-    #endif
-
-    #if defined(USE_WS2812B_1) || defined(USE_WS2812B_2)
-    [eTimerDriver_TIM5] = {
-        .periph = TIM5,
-        .prescaler = 0,
-        .counter_mode = LL_TIM_COUNTERMODE_UP,
-        .auto_reload = 125,
-        .clock_division = LL_TIM_CLOCKDIVISION_DIV1,
-        .enable_clock_fp = LL_APB1_GRP1_EnableClock,
-        .clock = LL_APB1_GRP1_PERIPH_TIM5,
-        .clock_source_fp = LL_TIM_SetClockSource,
-        .clock_source = LL_TIM_CLOCKSOURCE_INTERNAL,
-        .enable_interupt = false,
-        .auto_relead_preload_fp = LL_TIM_EnableARRPreload,
-        .master_slave_mode_fp = LL_TIM_DisableMasterSlaveMode,
-        .set_trigger = LL_TIM_SetTriggerOutput,
-        .triger_sync = LL_TIM_TRGO_RESET
-    },
-    #endif
-};
-/* clang-format on */
 
 /**********************************************************************************************************************
  * Private variables
  *********************************************************************************************************************/
 
+static sTimerDesc_t g_timer_lut[eTimer_Last] = {0};
 static bool g_is_all_timers_init = false;
-
-/* clang-format off */
-static bool g_is_counter_enable[eTimerDriver_Last] = {
-    [eTimerDriver_TIM10] = false,
-
-    #if defined(USE_MOTOR_A) || defined(USE_MOTOR_B) || defined(USE_PULSE_LED)
-    [eTimerDriver_TIM3] = false,
-    #endif
-
-    #if defined(USE_WS2812B_1) || defined(USE_WS2812B_2)
-    [eTimerDriver_TIM5] = false,
-    #endif
-};
-/* clang-format on */
+static bool g_is_counter_enable[eTimer_Last] = {false};
 
 /**********************************************************************************************************************
  * Exported variables and references
@@ -137,57 +47,62 @@ bool Timer_Driver_InitAllTimers (void) {
         return true;
     }
 
-    if (eTimerDriver_Last == 1) {
-        return false;
-    }
-
     g_is_all_timers_init = true;
 
     LL_TIM_InitTypeDef timer_init_struct = {0};
 
-    for (eTimerDriver_t timer = (eTimerDriver_First + 1); timer < eTimerDriver_Last; timer++) {
-        g_static_timer_lut[timer].enable_clock_fp(g_static_timer_lut[timer].clock);
+    for (eTimer_t timer = eTimer_First; timer < eTimer_Last; timer++) {
+        const sTimerDesc_t *desc = Timer_Config_GetTimerDesc(timer);
 
-        if (g_static_timer_lut[timer].enable_interupt) {
-            NVIC_SetPriority(g_static_timer_lut[timer].nvic, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 5, 0));
-            NVIC_EnableIRQ(g_static_timer_lut[timer].nvic);
+        if (desc == NULL) {
+            g_is_all_timers_init = false;
+            return false;
         }
 
-        timer_init_struct.Prescaler = g_static_timer_lut[timer].prescaler;
-        timer_init_struct.CounterMode = g_static_timer_lut[timer].counter_mode;
-        timer_init_struct.Autoreload = g_static_timer_lut[timer].auto_reload;
-        timer_init_struct.ClockDivision = g_static_timer_lut[timer].clock_division;
+        g_timer_lut[timer] = *desc;
+
+        g_timer_lut[timer].enable_clock_fp(g_timer_lut[timer].clock);
+
+        if (g_timer_lut[timer].enable_interupt) {
+            NVIC_SetPriority(g_timer_lut[timer].nvic, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 5, 0));
+            NVIC_EnableIRQ(g_timer_lut[timer].nvic);
+        }
+
+        timer_init_struct.Prescaler = g_timer_lut[timer].prescaler;
+        timer_init_struct.CounterMode = g_timer_lut[timer].counter_mode;
+        timer_init_struct.Autoreload = g_timer_lut[timer].auto_reload;
+        timer_init_struct.ClockDivision = g_timer_lut[timer].clock_division;
         
-        if (LL_TIM_Init(g_static_timer_lut[timer].periph, &timer_init_struct) == ERROR) {
+        if (LL_TIM_Init(g_timer_lut[timer].periph, &timer_init_struct) == ERROR) {
             g_is_all_timers_init = false;
         }
         
-        if (g_static_timer_lut[timer].clock_source_fp != NULL) {
-            g_static_timer_lut[timer].clock_source_fp(g_static_timer_lut[timer].periph, g_static_timer_lut[timer].clock_source);
+        if (g_timer_lut[timer].clock_source_fp != NULL) {
+            g_timer_lut[timer].clock_source_fp(g_timer_lut[timer].periph, g_timer_lut[timer].clock_source);
         }
 
-        if (g_static_timer_lut[timer].auto_relead_preload_fp != NULL) {
-            g_static_timer_lut[timer].auto_relead_preload_fp(g_static_timer_lut[timer].periph);
+        if (g_timer_lut[timer].auto_relead_preload_fp != NULL) {
+            g_timer_lut[timer].auto_relead_preload_fp(g_timer_lut[timer].periph);
         }
 
-        if (g_static_timer_lut[timer].master_slave_mode_fp != NULL) {
-            g_static_timer_lut[timer].master_slave_mode_fp(g_static_timer_lut[timer].periph);
+        if (g_timer_lut[timer].master_slave_mode_fp != NULL) {
+            g_timer_lut[timer].master_slave_mode_fp(g_timer_lut[timer].periph);
         }
 
-        if (g_static_timer_lut[timer].set_slave_mode_fp != NULL) {
-            g_static_timer_lut[timer].set_slave_mode_fp(g_static_timer_lut[timer].periph, g_static_timer_lut[timer].slave_mode);
+        if (g_timer_lut[timer].set_slave_mode_fp != NULL) {
+            g_timer_lut[timer].set_slave_mode_fp(g_timer_lut[timer].periph, g_timer_lut[timer].slave_mode);
         }
 
-        if (g_static_timer_lut[timer].set_trigger != NULL) {
-            g_static_timer_lut[timer].set_trigger(g_static_timer_lut[timer].periph, g_static_timer_lut[timer].triger_sync);
+        if (g_timer_lut[timer].set_trigger != NULL) {
+            g_timer_lut[timer].set_trigger(g_timer_lut[timer].periph, g_timer_lut[timer].triger_sync);
         }
     }
 
     return g_is_all_timers_init;
 }
 
-bool Timer_Driver_Start (const eTimerDriver_t timer) {
-    if ((timer <= eTimerDriver_First) || (timer >= eTimerDriver_Last)) {
+bool Timer_Driver_Start (const eTimer_t timer) {
+    if (!Timer_Config_IsCorrectTimer(timer)) {
         return false;
     }
 
@@ -196,7 +111,7 @@ bool Timer_Driver_Start (const eTimerDriver_t timer) {
     }
 
     if (!g_is_counter_enable[timer]) {
-        LL_TIM_EnableCounter(g_static_timer_lut[timer].periph);
+        LL_TIM_EnableCounter(g_timer_lut[timer].periph);
 
         g_is_counter_enable[timer] = true;
     }
@@ -204,8 +119,8 @@ bool Timer_Driver_Start (const eTimerDriver_t timer) {
     return true;
 }
 
-bool Timer_Driver_Stop (const eTimerDriver_t timer) {
-    if ((timer <= eTimerDriver_First) || (timer >= eTimerDriver_Last)) {
+bool Timer_Driver_Stop (const eTimer_t timer) {
+    if (!Timer_Config_IsCorrectTimer(timer)) {
         return false;
     }
 
@@ -214,9 +129,9 @@ bool Timer_Driver_Stop (const eTimerDriver_t timer) {
     }
 
     if (g_is_counter_enable[timer]) {
-        LL_TIM_DisableCounter(g_static_timer_lut[timer].periph);
+        LL_TIM_DisableCounter(g_timer_lut[timer].periph);
 
-        LL_TIM_SetCounter(g_static_timer_lut[timer].periph, 0);
+        LL_TIM_SetCounter(g_timer_lut[timer].periph, 0);
 
         g_is_counter_enable[timer] = false;
     }
@@ -224,10 +139,12 @@ bool Timer_Driver_Stop (const eTimerDriver_t timer) {
     return true;
 }
 
-uint16_t Timer_Driver_GetResolution (const eTimerDriver_t timer) {
-    if ((timer <= eTimerDriver_First) || (timer >= eTimerDriver_Last)) {
+uint16_t Timer_Driver_GetResolution (const eTimer_t timer) {
+    if (!Timer_Config_IsCorrectTimer(timer)) {
         return 0;
     }
 
-    return LL_TIM_GetAutoReload(g_static_timer_lut[timer].periph);
+    return LL_TIM_GetAutoReload(g_timer_lut[timer].periph);
 }
+
+#endif /* ENABLE_TIMER */
