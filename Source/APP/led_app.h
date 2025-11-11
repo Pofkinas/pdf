6733417c46d43a60cd39a7d43364e318b89d070b
Pofkinas <povilaszvi358@gmail.com>
2025-11-11 19:43:26 +0200

    feat(base/led): export led configuration

--- Source/APP/led_app.h
+++ Source/APP/led_app.h
@@ -4,10 +4,13 @@
  * Includes
  *********************************************************************************************************************/
 
+#include "framework_config.h"
+
+#if defined(ENABLE_LED) || defined(ENABLE_PWM_LED)
 #include <stdbool.h>
 #include <stdint.h>
 #include "led_api.h"
-#include "framework_config.h"
+#include "led_config.h"
 
 /**********************************************************************************************************************
  * Exported definitions and macros
@@ -21,17 +24,17 @@
 typedef enum eLedTask {
     eLedTask_First = 0,
     
-    #ifdef USE_LED
+    #ifdef ENABLE_LED
     eLedTask_Set,
     eLedTask_Reset,
     eLedTask_Toggle,
     eLedTask_Blink,
-    #endif
+    #endif /* ENABLE_LED */
 
-    #ifdef USE_PWM_LED
+    #ifdef ENABLE_PWM_LED
     eLedTask_Set_Brightness,
     eLedTask_Pulse,
-    #endif
+    #endif /* ENABLE_PWM_LED */
     
     eLedTask_Last
 } eLedTask_t;
@@ -45,12 +48,15 @@ typedef struct sLedCommon {
     eLed_t led;
 } sLedCommon_t;
 
+#ifdef ENABLE_LED
 typedef struct sLedBlink {
     eLed_t led;
     uint8_t blink_time;
     uint16_t blink_frequency;
 } sLedBlink_t;
+#endif /* ENABLE_LED */
 
+#ifdef ENABLE_PWM_LED
 typedef struct sLedSetBrightness {
     eLedPwm_t led;
     uint8_t duty_cycle;
@@ -61,6 +67,7 @@ typedef struct sLedPulse {
     uint8_t pulse_time;
     uint16_t pulse_frequency;
 } sLedPulse_t;
+#endif /* ENABLE_PWM_LED */
 /* clang-format on */
 
 /**********************************************************************************************************************
@@ -74,4 +81,5 @@ typedef struct sLedPulse {
 bool LED_APP_Init (void);
 bool LED_APP_Add_Task (sLedCommandDesc_t *task_to_message_queue);
 
+#endif /* defined(ENABLE_LED) || defined(ENABLE_PWM_LED) */
 #endif /* SOURCE_APP_LED_APP_H_ */
