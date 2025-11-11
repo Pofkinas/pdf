cc2239a1f8f3afc31d3cdbbd7f21800fe00e47a2
Pofkinas <povilaszvi358@gmail.com>
2025-11-11 19:39:30 +0200

    feat: export i2c configuration

--- Source/Driver/i2c_driver.h
+++ Source/Driver/i2c_driver.h
@@ -4,48 +4,36 @@
  * Includes
  *********************************************************************************************************************/
 
+#include "framework_config.h"
+
+#ifdef ENABLE_I2C
 #include <stdbool.h>
 #include <stdint.h>
-#include "framework_config.h"
+#include "i2c_config.h"
 
 /**********************************************************************************************************************
  * Exported definitions and macros
  *********************************************************************************************************************/
 
-#define I2C_WRITE 0
-#define I2C_READ  1
-
 /**********************************************************************************************************************
  * Exported types
  *********************************************************************************************************************/
 
-/* clang-format off */
-typedef enum eI2cDriver {
-    eI2cDriver_First = 0,
-    
-    #ifdef USE_I2C1
-    eI2cDriver_1,
-    #endif
+typedef enum eI2c_Flags {
+    eI2c_Flags_First = 0,
+    eI2c_Flags_Busy = eI2c_Flags_First,
+    eI2c_Flags_StartBit,
+    eI2c_Flags_Addr,
+    eI2c_Flags_Txe,
+    eI2c_Flags_ByteTransferFinish,
+    eI2c_Flags_Rxne,
+    eI2c_Flags_AckFailure,
+    eI2c_Flags_BusError,
+    eI2c_Flags_BusReset,
+    eI2c_Flags_Last
+} eI2c_Flags_t;
 
-    eI2cDriver_Last
-} eI2cDriver_t;
-
-typedef enum eI2cDriver_Flags {
-    eI2cDriver_Flags_First = 0,
-    eI2cDriver_Flags_Busy = eI2cDriver_Flags_First,
-    eI2cDriver_Flags_StartBit,
-    eI2cDriver_Flags_Addr,
-    eI2cDriver_Flags_Txe,
-    eI2cDriver_Flags_ByteTransferFinish,
-    eI2cDriver_Flags_Rxne,
-    eI2cDriver_Flags_AckFailure,
-    eI2cDriver_Flags_BusError,
-    eI2cDriver_Flags_BusReset,
-    eI2cDriver_Flags_Last
-} eI2cDriver_Flags_t;
-/* clang-format on */
-
-typedef void (*i2c_callback_t) (const eI2cDriver_Flags_t flag, void *context);
+typedef void (*i2c_callback_t) (const eI2c_Flags_t flag, void *context);
 
 /**********************************************************************************************************************
  * Exported variables
@@ -55,17 +43,18 @@ typedef void (*i2c_callback_t) (const eI2cDriver_Flags_t flag, void *context);
  * Prototypes of exported functions
  *********************************************************************************************************************/
 
-bool I2C_Driver_Init (const eI2cDriver_t i2c, i2c_callback_t flag_callback, void *context);
-bool I2C_Driver_EnableIt (const eI2cDriver_t i2c);
-bool I2C_Driver_DisableIt (const eI2cDriver_t i2c);
-bool I2C_Driver_StartComms (const eI2cDriver_t i2c, const uint8_t address, const uint8_t rw_operation);
-bool I2C_Driver_StopComms (const eI2cDriver_t i2c);
-bool I2C_Driver_Acknowledge (const eI2cDriver_t i2c, const bool ack);
-bool I2C_Driver_SendByte (const eI2cDriver_t i2c, const uint8_t data);
-bool I2C_Driver_ReadByte (const eI2cDriver_t i2c, uint8_t *data);
-bool I2C_Driver_ReadByteAck (const eI2cDriver_t i2c, uint8_t *data, const bool ack);
-bool I2C_Driver_CheckFlag (const eI2cDriver_t i2c, const eI2cDriver_Flags_t flag);
-void I2C_Driver_ClearFlag (const eI2cDriver_t i2c, const eI2cDriver_Flags_t flag);
-bool I2C_Driver_ResetLine (const eI2cDriver_t i2c);
+bool I2C_Driver_Init (const eI2c_t i2c, i2c_callback_t flag_callback, void *context);
+bool I2C_Driver_EnableIt (const eI2c_t i2c);
+bool I2C_Driver_DisableIt (const eI2c_t i2c);
+bool I2C_Driver_StartComms (const eI2c_t i2c, const uint8_t address, const uint8_t rw_operation);
+bool I2C_Driver_StopComms (const eI2c_t i2c);
+bool I2C_Driver_Acknowledge (const eI2c_t i2c, const bool ack);
+bool I2C_Driver_SendByte (const eI2c_t i2c, const uint8_t data);
+bool I2C_Driver_ReadByte (const eI2c_t i2c, uint8_t *data);
+bool I2C_Driver_ReadByteAck (const eI2c_t i2c, uint8_t *data, const bool ack);
+bool I2C_Driver_CheckFlag (const eI2c_t i2c, const eI2c_Flags_t flag);
+void I2C_Driver_ClearFlag (const eI2c_t i2c, const eI2c_Flags_t flag);
+bool I2C_Driver_ResetLine (const eI2c_t i2c);
 
+#endif /* ENABLE_I2C */
 #endif /* SOURCE_DRIVER_I2C_DRIVER_H_ */
